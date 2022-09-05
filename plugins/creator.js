4ddfcb24bc2daf7831a4c6bf0c9315fce722d172
naufalmuflih123 <112526698+naufalmuflih123@users.noreply.github.com>
2022-09-05 21:35:55 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
+FN: ᴹᴿ᭄ WAbotsOfcc_1ঔৣ꧂
+item.ORG: ᴹᴿ᭄ WabotOffcོ_1ঔৣ꧂
+item1.TEL;waid=6289601124991:6289601124991@s.whatsapp.net
 item1.X-ABLabel:owner selalu ada 
-item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
+item2.YOUTUBE;type=INTERNET: https://www.youtube.com/channel/UC1esKQw-DKM5Xa5hF7vqdGA
 item2.X-ABLabel:YOUTUBE
 item3.ADR:;;🎍AMERIKA SERIKAT;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
-item4.URL:https//github.com/Alpiii22 
+item4.URL:https//github.com/wa_botz 
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
