536d5f4e5b7b5927ab040236b2fd13bf29248184
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-09 12:48:44 +0700

    Update sourcode.js

--- plugins/sourcode.js
+++ plugins/sourcode.js
@@ -3,17 +3,7 @@
 
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/239f046a928370340401f.jpg',
-`Hi Kak @${m.sender.split('@')[0]} 
-
-◪ 📮 *SCRIPT BOT*
-│ *Script :* 
-│ ╰ https://youtu.be/FHntJPk2PkU
-│ *Mampir Youtube Owner Banh:* 
-│ ╰ https://tinyurl.com/2qsejxnw
-╰──────────═┅═──────────
-`,`📍 *N o t e :* 
-• Jangan lupa mampir Youtube Owner Kak!
-• Jangan Lupa kasih star & kasih credit
+`𝕭𝖔𝖙
 
 Official By @${'0'.split('@')[0]}
 Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
