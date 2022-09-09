00f5a43a83ef974e451b23993fbb43ef888888ab
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-09 12:45:15 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -5,31 +5,8 @@ let dana = global.dana
 let pulsa = global.pulsa
 let saweria = global.saweria
 let owner = global.owner
-let anu = `Hallo mypren 👋
-━━━〔 ıll *Sewa Bot* llı 〕━━ꕥ
-━━━━━━━━━━━━━━━━━━━━
-━━━〔 ıll *HARGA* llı 〕━━ꕥ
-⬡ *1 BULAN:* *20.000*
-⬡ *2 BULAN:* *30.000*
-⬡ *1 BULAN 2/3 GRUP:* *40.000*
-⬡ *lebih dari 3 tambah +10.000*
-┗━━━━━━━━━━━━━━━ꕥ
-┏━━━〔 ıll *PAYMENT* llı 〕━━ꕥ
-⬡ *DANA:* ${dana}
-⬡ *PULSA:* ${pulsa}
-┗━━━━━━━━━━━━━━━━━━ꕥ
-┏━━〔 ıll *RULES* llı 〕━ꕥ
-⬡ [❗] *Dana yang sudah masuk tidak bisa di kembalikan*
-┗━━━━━━━━━━ꕥ
-Jika anda berminat hubungi nomor di bawah!!
-
-⟩» *jangan lupa donasi kak* «⟨
-Terimakasih yang sudah mendonasikan untuk bot
-
-Contact person Sewa Bot:
-wa.me/${6285850539404} (Owner)
-
-*Follow Instagram ku juga kak😼*`
+let anu = `Ga Butuh Pulsa/Dana :v
+By : 𝕭𝖔𝖙`
   conn.send3ButtonImg(m.chat, fla + teks, anu, instagram, 'Pilihan Store', '.pilihanstore', 'Owner', '.owner', 'Menu', '.menu', m) 
 }
 handler.help = ['sewaoscar']
